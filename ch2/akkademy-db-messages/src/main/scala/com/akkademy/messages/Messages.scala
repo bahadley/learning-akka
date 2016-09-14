f1f87d19f2eed425ae6ee122a8fd661b7a240b19
Ben Hadley <bhadley2@gmail.com>
2016-09-14 10:08:33 -0700

    Change Object type to Any

--- ch2/akkademy-db-messages/src/main/scala/com/akkademy/messages/Messages.scala
+++ ch2/akkademy-db-messages/src/main/scala/com/akkademy/messages/Messages.scala
@@ -1,6 +1,6 @@
 package com.akkademy.messages
 
-case class SetRequest(key: String, value: Object)
+case class SetRequest(key: String, value: Any)
 case class GetRequest(key: String)
 
 case class KeyNotFoundException(key: String) extends Exception
