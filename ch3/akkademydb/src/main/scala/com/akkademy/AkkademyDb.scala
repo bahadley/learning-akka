ad1815cc576f98a93e91784a9f908c4c84b5e4b1
bahadley <bhadley2@gmail.com>
2016-09-17 16:55:24 -0700

    Use tell pattern with Decrypter and simplify logging messages

--- ch3/akkademydb/src/main/scala/com/akkademy/AkkademyDb.scala
+++ ch3/akkademydb/src/main/scala/com/akkademy/AkkademyDb.scala
@@ -10,34 +10,32 @@ class AkkademyDb extends Actor with ActorLogging {
 
   val map = new HashMap[String, Any]
 
-  val name = self.path.name
-
   def receive = LoggingReceive {
 
     case msg: Get =>
-      log.info("{}: Rcvd Get(key: [{}])", name, msg.key)
+      log.info("Rcvd Get(key: [{}])", msg.key)
       map.get(msg.key) match {
         case Some(value) => sender() ! value 
         case None        => sender() ! Status.Failure(KeyNotFound(msg.key))
       }
 
     case msg: Set =>
-      log.info("{}: Rcvd Set(key: [{}], value: [{}])", name, msg.key, msg.value)
+      log.info("Rcvd Set(key: [{}], value: [{}])", msg.key, msg.value)
       sender() ! map.put(msg.key, msg.value)
 
     case msg: SetIfNotExists =>
-      log.info("{}: Rcvd SetIfNotExists(key: [{}], value: [{}])", name, msg.key, msg.value)
+      log.info("Rcvd SetIfNotExists(key: [{}], value: [{}])", msg.key, msg.value)
       sender() ! map.getOrElseUpdate(msg.key, msg.value)
 
     case msg: Delete =>
-      log.info("{}: Rcvd Delete(key: [{}])", name, msg.key)
+      log.info("Rcvd Delete(key: [{}])", msg.key)
       map.remove(msg.key) match {
         case Some(value) => sender() ! value 
         case None        => sender() ! Status.Failure(KeyNotFound(msg.key))
       }
 
     case msg: Clear =>
-      log.info("{}: Rcvd Clear", name)
+      log.info("Rcvd Clear")
       map.clear 
       sender() ! Status.Success
 
--- ch3/ask-await/src/main/scala/AskAwait.scala
+++ ch3/ask-await/src/main/scala/AskAwait.scala
@@ -4,7 +4,7 @@ import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.event.LoggingReceive
 import akka.pattern.ask
 import akka.util.Timeout
-import com.akkademy.messages._
+import com.akkademy.messages.{Get, Set}
 import com.akkademy.AkkademyDb
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
@@ -12,44 +12,11 @@ import scala.language.postfixOps
 
 
 case class Test(id: String)
-case class Decrypt(cipherText: String)
-
-
-class DecryptionService(cachePath: String) extends Actor with ActorLogging {
-
-  private val cache = context.actorSelection(cachePath) 
-  private val decrypter = context.actorOf(Props[Decrypter], name = "decrypter")
-
-  private implicit val timeout = Timeout(2 seconds)
-
-  def receive = LoggingReceive {
-    case msg: Test =>
-      log.info("Rcvd Test(id: [{}])", msg.id)
-
-      val f1: Future[String] = ask(cache, Get(msg.id)).mapTo[String]
-      val ct = Await.result(f1, timeout.duration)
-
-      log.info("ciphertext: [{}]", ct)
-
-      val f2: Future[String] = ask(decrypter, Decrypt(ct)).mapTo[String]
-      val pt = Await.result(f2, timeout.duration) 
-
-      log.info("plaintext: [{}]", pt)
-  }
-}
-
-
-class Decrypter extends Actor with ActorLogging {
-  def receive = LoggingReceive {
-    case msg: Decrypt =>
-      log.info("Rcvd Decrypt(cipherText: [{}])", msg.cipherText)
-      sender() ! msg.cipherText.replace("(encryption)", "")
-  }
-}
+case class DecryptCipherText(cipherText: String)
+case class DecryptResponse(plainText: String)
 
 
 object Main extends App {
-
   val system = ActorSystem("ask-await")
   val cache = system.actorOf(Props[AkkademyDb], name = "cache")
   val service = system.actorOf(Props(classOf[DecryptionService], cache.path.toString), name = "service")
@@ -57,10 +24,39 @@ object Main extends App {
   val id = "1"
 
   // Pre-populate the cache
-  cache ! Set(id, "(encryption)Be sure to drink your Ovaltine")
+  cache ! Set(id, "(encryption)Viva Akka!")
 
   service ! Test(id) 
 
-  Thread.sleep(2000)
+  Thread.sleep(1000)
   system.terminate
 }
+
+
+class DecryptionService(cachePath: String) extends Actor with ActorLogging {
+  private val cache = context.actorSelection(cachePath) 
+  private val decrypter = context.actorOf(Props[Decrypter], name = "decrypter")
+  private implicit val timeout = Timeout(2 seconds)
+
+  def receive = LoggingReceive {
+    case msg: Test =>
+      log.info("Rcvd Test(id: [{}])", msg.id)
+      val f1: Future[String] = ask(cache, Get(msg.id)).mapTo[String]
+      val ct = Await.result(f1, timeout.duration)
+      log.info("ciphertext: [{}]", ct)
+      decrypter ! DecryptCipherText(ct)
+
+    case msg: DecryptResponse =>
+      log.info("plaintext: [{}]", msg.plainText)
+  }
+}
+
+class Decrypter extends Actor with ActorLogging {
+  def receive = LoggingReceive {
+    case msg: DecryptCipherText =>
+      log.info("Rcvd Decrypt(cipherText: [{}])", msg.cipherText)
+      val plainText = msg.cipherText.replace("(encryption)", "")
+      sender() ! DecryptResponse(plainText) 
+  }
+}
+
